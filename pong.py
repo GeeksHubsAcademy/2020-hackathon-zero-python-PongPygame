a1795b17c2681cbbebd2d5da30f214f5239cee7a
Manuel S. Lemos <m.lemos@geekshubs.com>
2020-05-11 10:58:59 +0200

    Draw UI

--- pong.py
+++ pong.py
@@ -12,6 +12,14 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_width))
 pygame.display.set_caption('Pong')
 
+# Game Rectangles
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200, 200, 200)
+
 while True:
     #Handling input
     for event in pygame.event.get():
@@ -19,6 +27,13 @@ while True:
             pygame.quit()
             sys.exit()
 
+    #Visuals
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2, screen_height))
+    
     # Updating the Windows
     pygame.display.flip()
     clock.tick(60)
